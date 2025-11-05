6ff09657172619d6ad3247973c42e6f87ef0edce
Abhishek Raj <abhishek4code@gmail.com>
2025-11-05 21:15:09 +0530

    chore: fixed deployment build ie removed unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Toaster, toast } from "sonner" 
 import React, { useState, useEffect } from "react"
 import {
